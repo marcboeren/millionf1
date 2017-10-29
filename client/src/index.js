8c312eb1c29fafe60b24f80fa2828f5bf3b91e40
Marc Boeren <marc@million.nl>
2017-10-29 16:31:46 +0100

    ok, base apollo client works

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:3001/graphql' }),
+  cache: new InMemoryCache()
+});
+
+var season_gql = gql`query {
+  season {
+    year,
+    races {
+      round,
+      name,
+      results {
+        position,
+        number,
+        driver {
+          id,
+          code,
+          name,
+          givenName,
+        },
+        points,
+        grid,
+      },
+    },
+  }
+}`;
+client.query({ query: season_gql }).then(console.log);
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
 registerServiceWorker();
--- server/app.js
+++ server/app.js
@@ -3,11 +3,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import schema from './data/schema';
-
+import cors from 'cors';
 
 const PORT = 3001;
 
 const app = express();
+
+//FIXES CORS ERROR
+var whitelist = [
+    'http://localhost:3000',
+];
+var corsOptions = {
+    origin: function(origin, callback){
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
+
 
 // bodyParser is needed just for POST.
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
